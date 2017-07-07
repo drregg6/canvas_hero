ca33fa187670b0df4a1f1b253aeeab0c7087add7
Dave <drregg6@gmail.com>
2017-07-07 11:29:53 -0400

    commented the code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,26 @@
-var canvas = document.querySelector('canvas');
-var cxt = canvas.getContext('2d');
-var colorsArr = ['#f2385a', '#f5a503', '#e9f1df', '#4ad9d9', '#36b1bf'];
-var circlesArr = [];
-var circles = 1000;
-var mouseObj = {
+// variables declaration
+var canvas, cxt; // canvas
+var colorsArr, circlesArr; // arrays
+var circles, minRadius, maxRadius, mouseObj; // constants
+
+canvas = document.querySelector('canvas');
+cxt = canvas.getContext('2d');
+
+colorsArr = ['#f2385a', '#f5a503', '#e9f1df', '#4ad9d9', '#36b1bf'];
+circlesArr = [];
+circles = 1400;
+minRadius = 4;
+maxRadius = 35;
+mouseObj = {
     x: undefined,
     y: undefined
 }
 
+// set the height and width of the canvas
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-animate();
-init();
 
+// resize accordingly
 window.addEventListener('resize', function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
@@ -25,12 +33,17 @@ window.addEventListener('mousemove', function(ev) {
     mouseObj.y = ev.y;
 });
 
+// mini function
 function randRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+
+// spawn new random circles with each resize
 function init() {
     circlesArr = [];
+    // for the amount of circles you want
+    // push a new circle of random location and speed into an arr
     for (var i = 0; i < circles; i++) {
         var x = Math.random() * window.innerWidth;
         var y = Math.random() * window.innerHeight;
@@ -42,15 +55,18 @@ function init() {
     }
 }
 
+
+// Circle object declared
 function Circle(x, y, radius, dx, dy) {
     this.x = x;
     this.y = y;
-    this.radius = 5;
+    this.radius = minRadius;
     this.dx = dx;
     this.dy = dy;
     this.arcEnd = Math.PI * 2;
     this.color = colorsArr[Math.floor(Math.random() * colorsArr.length)];
     
+    // draw the circle
     this.draw = function() {
         cxt.beginPath();
         cxt.arc(this.x, this.y, this.radius, this.arcEnd, false);
@@ -58,6 +74,7 @@ function Circle(x, y, radius, dx, dy) {
         cxt.fill();
     }
     
+    // move the circle's x and y locations based on the dx and dy
     this.move = function() {
         if (this.x - this.radius > window.innerWidth) {
             this.x = 0 - this.radius;
@@ -75,23 +92,34 @@ function Circle(x, y, radius, dx, dy) {
         this.x += this.dx;
         this.y += this.dy;
         
+        // interact as the mouse approaches the circle
         if (mouseObj.x - this.x < 50 && mouseObj.x - this.x > -50 && mouseObj.y - this.y < 50 && mouseObj.y - this.y > -50) {
-            if (this.radius < 40) {
+            if (this.radius < maxRadius) {
                 this.radius += 1;
             }
-        } else if (this.radius > 5) {
+        } else if (this.radius > minRadius) {
             this.radius -= 1;
         }
         
+        // redraw the circle with each animation loop
         this.draw();
     }
 }
 
+
+// animate in a continuous loop
 function animate() {
     requestAnimationFrame(animate);
+    // clear the canvas each time animation is run
     cxt.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
+    
+    // move each circle in the arr
     circlesArr.forEach(function(circle) {
         circle.move();
     });
 }
+
+
+// on load
+animate();
+init();
